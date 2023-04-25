482eab06ce5fb2fa06ebe0fa7d45ae99ddf00e70
yuanshuai <yuanshuai@gyyx.cn>
2023-04-25 13:10:45 +0800

    server

---
+++ yyss-server/src/main/java/vip/yuanshuai/YyssServerApplication.java
@@ -0,0 +1,13 @@
+package vip.yuanshuai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YyssServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YyssServerApplication.class, args);
+	}
+
+}
